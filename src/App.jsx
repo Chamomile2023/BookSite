97bed9ba0aef6d4f77d37c0a9622e0cf19cafe7b
Sarvinoz <fullstackdevelop2023@gmail.com>
2022-09-03 13:54:16 +0500

    Part of thr login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import "./App.scss";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Login from "./components/Login/Login";
 import Main from "./components/Main/Main";
 
 const App = () => {
+  //Login Page
+  const [auth, setAuth] = useState(false);
+  const user = {
+    user: "Admin123",
+    password: "123456",
+  };
+  const navigate = useNavigate();
+  const useAuth = (params) => {
+    if (user.user === params.user && user.password === params.password ? navigate("/") : navigate("/login")) {
+      setAuth(true);
+    } else {
+      setAuth(false);
+    }
+  };
   return (
     <>
       <Routes>
-        <Route index path="/login" element={<Login />} />
+        <Route
+          index
+          path="/login"
+          element={<Login auth={auth} useAuth={useAuth} />}
+        />
         <Route path="/" element={<Main />} />
       </Routes>
     </>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import './Login.scss'
+import "./Login.scss";
+import { useState } from "react";
 import User from "../assets/user.svg";
 import Lock from "../assets/lock.svg";
 import Logo from "../assets/LOGO.svg";
 
-const Login = () => {
+const Login = ({ useAuth }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const params = {
+        username: username,
+        password: password,
+    };
     return (
         <>
             <div className="login">
@@ -16,15 +23,27 @@ const Login = () => {
                         <div className="login__inputs">
                             <div className="login__user">
                                 <img src={User} alt="" className="login__user--icon" />
-                                <input type="text" className="login__user--user" placeholder="Username" />
+                                <input
+                                    type="text"
+                                    className="login__user--user"
+                                    placeholder="Username"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
                             </div>
                             <div className="login__password">
                                 <img src={Lock} alt="" className="login__user--icon" />
-                                <input type="password" className="login__password--password" placeholder="password" />
+                                <input
+                                    type="password"
+                                    className="login__password--password"
+                                    placeholder="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
                             </div>
                         </div>
                         <div className="login__btn">
-                            <button className="login__btn--btn">LOGIN</button>
+                            <button className="login__btn--btn" onClick={() => useAuth(params)}>LOGIN</button>
                         </div>
                     </div>
                 </div>
